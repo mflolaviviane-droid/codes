6db3bc1b2ac4de21f18a53fbb262fcc7e327840f
mflolaviviane-droid <mf.lola.viviane@gmail.com>
2026-05-17 22:39:56 -0300

    cpf2.py

---
+++ cpf2.py
@@ -0,0 +1,19 @@
+# Cálculo do 11º número do CPF
+
+cpf = input("Digite os 10 primeiros números do CPF: ")
+
+soma = 0
+peso = 11
+
+for numero in cpf:
+    soma += int(numero) * peso
+    peso -= 1
+
+resto = soma % 11
+
+if resto < 2:
+    dv2 = 0
+else:
+    dv2 = 11 - resto
+
+print("O 11º número do CPF é:", dv2)
