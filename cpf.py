112070fbedc501c6bb9b68bd404586c81cce0e9f
mflolaviviane-droid <mf.lola.viviane@gmail.com>
2026-05-17 22:38:15 -0300

    cpf.py

---
+++ cpf.py
@@ -0,0 +1,19 @@
+ Verificador de CPF - Primeiro dígito
+
+cpf = int(input("Digite os 9 primeiros números do CPF: "))
+
+soma = 0
+
+for m in range(2, 11):
+    n = cpf % 10
+    soma = soma + (n * m)
+    cpf = cpf // 10
+
+dv1 = soma % 11
+
+if dv1 >= 2:
+    dv1 = 11 - dv1
+else:
+    dv1 = 0
+
+print("Primeiro dígito verificador:", dv1)
